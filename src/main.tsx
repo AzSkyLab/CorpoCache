85cdc86a318a3906d113387a60720a1a713fee53
csGIT34 <103604454+csGIT34@users.noreply.github.com>
2025-05-02 22:33:29 -0400

    vitemigrate

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// This file was renamed from index.tsx for Vite compatibility
